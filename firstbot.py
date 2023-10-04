a079a796be33033cb7fbcb78376586734877c05e
Midori Fukami <midori.fukami@hotmail.com>
2023-10-04 23:00:14 +0200

    Update firstbot.py

--- firstbot.py
+++ firstbot.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE, message: types.Message):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)  
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     caps_handler = CommandHandler('caps', caps)  
     inline_caps_handler = InlineQueryHandler(inline_caps)
 
